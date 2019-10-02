945b6ebcd66f31c6d53c8443248c508742492162
daisy Wen <wen_daisy@163.com>
2019-10-02 13:41:39 +0800

    init app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+export default class App extends Component{
+  render(){
+    return(
+      <div style={{color:'#ccc'}}>
+        王雯你要好好加油啊！
+      </div>
+    )
+  }
+}
+
---
+++ src/README.md
@@ -0,0 +1,7 @@
+目录设计说明
+api         ajax相关
+assets      公共资源
+components  非路由组件
+config      配置
+pages       路由组件
+utils       工具模块
