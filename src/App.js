f3760021b0d93b2320cc0fb39d8e8840a31d0b36
ww <wen_daisy@163.com>
2019-11-15 17:11:05 +0800

    mege master modify

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ export default class App extends Component{
   render(){
     return(
       <div style={{color:'#ccc'}}>
-        四国是什么地方？
+        四国是什么地方？merge
+        。。。
       </div>
     )
   }
